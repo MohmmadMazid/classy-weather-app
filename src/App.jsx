87951a0b7ecd49a32212428b7bd1f34aead77a55
MohmmadMazid <mazidmansury5500@gmail.com>
2026-03-09 13:22:51 +0530

    component life cycle method learned and used

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Weather from "./Weather";
 import Input from "./Input";
 class App extends React.Component {
   state = {
-    location: "New Delhi",
+    location: "",
     loading: false,
     displayLocation: "",
     weather: {},
@@ -22,6 +22,7 @@ class App extends React.Component {
   };
 
   fetchWeather = async () => {
+    if (this.state.location.length <= 3) return this.setState({ weather: "" }); // because of this line component will unmount function will runn when the length of the location will be less then then the 3 or equal to three
     try {
       this.setState({ loading: true });
       // 1) Getting location (geocoding)
@@ -53,6 +54,32 @@ class App extends React.Component {
     }
   };
 
+  // this is the component did mount method for the class based coponents it will executed at the time when the component will
+  //  inialy painted by the browser or we can say the it will run when we open the application in browser
+
+  // this function will works like useEffect ,like when we pass useEffect dipendency array empty that mean that useEffect will
+  // run only the initial render then it will not run again
+  // this function also will run same
+  // useEffecct - []
+  componentDidMount() {
+    // now we do not need to fetch the data at the initial render because we are not passing any value initialy
+    // this.fetchWeather();
+    this.setState({
+      location: JSON.parse(localStorage.getItem("location")) || "",
+    });
+  }
+
+  // this function will  similar to the useEffect when we pass the dependency arry sum satate then whenever the state or dependency array will change
+  // that mean useEffect will run again
+  // this function will work similar whenever the previous satate will change then componnetDidUpdate function will run
+  // useEffecct - [prevprops ,prevstate]
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("location", JSON.stringify(this.state.location));
+    }
+  }
   render() {
     return (
       <div className="app">
@@ -65,7 +92,7 @@ class App extends React.Component {
             setLocation={this.handleInputValue}
           />
         </div>
-        <button onClick={this.fetchWeather}>get weather</button>
+        {/* <button onClick={this.fetchWeather}>get weather</button> */}
         {this.state.loading ? <p className="loader">Loading...</p> : ""}
         {this.state.weather.weathercode && (
           <Weather
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,24 +1,41 @@
-
 import React from "react";
 import Dates from "./Date";
 
+class Weather extends React.Component {
+  // this function will work similar to the useEffect cleanup function
+  // this will run only when the component will un-mount from the browsers screen
 
-class Weather extends React.Component{
+  componentWillUnmount() {
+    console.log("unmount the component");
+  }
 
-
-    render(){
-        const   {temperature_2m_max:max,temperature_2m_min:min,time:dates,weathercode:codes}=this.props.weather;
-        return(
-            <div>
-                <h2>Weather {this.props.location}</h2>
-                <ul className="weather">
-                    {dates.map((date,i)=>{
-                        return <Dates date={date} max={max.at(i)} min={min.at(i)} codes={codes.at(i)} isToday={i===0} key={date}/>
-                    })}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    const {
+      temperature_2m_max: max,
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather;
+    return (
+      <div>
+        <h2>Weather {this.props.location}</h2>
+        <ul className="weather">
+          {dates.map((date, i) => {
+            return (
+              <Dates
+                date={date}
+                max={max.at(i)}
+                min={min.at(i)}
+                codes={codes.at(i)}
+                isToday={i === 0}
+                key={date}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Weather;
